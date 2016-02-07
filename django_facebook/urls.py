2a60fad2934907771a8ded3965fa6946c50769fc
waelbeso <waelabbas@live.com>
2016-02-07 14:32:41 +0800

    avoiding deprecated warning
    avoiding deprecated warning in Django 1.9

--- django_facebook/urls.py
+++ django_facebook/urls.py
@@ -6,40 +6,28 @@ from django.conf import settings
 
 # help autodiscovery a bit
 from django_facebook import admin
+from django_facebook.views import connect,disconnect,example
+from django_facebook.example_views import lazy_decorator_example,decorator_example,decorator_example_scope,wall_post,checkins,image_upload,canvas,page_tab,open_graph_beta,remove_og_share
 
-urlpatterns = patterns(
-    'django_facebook.views',
-    url(r'^connect/$', 'connect', name='facebook_connect'),
-    url(r'^disconnect/$',
-        'disconnect', name='facebook_disconnect'),
-    url(r'^example/$', 'example', name='facebook_example'),
-)
+urlpatterns = [
+url(r'^connect/$', connect, name='facebook_connect'),
+url(r'^disconnect/$',disconnect, name='facebook_disconnect'),
+url(r'^example/$', example, name='facebook_example'),
+]
 
-dev_patterns = patterns(
-    'django_facebook.example_views',
-    url(
-        r'^lazy_decorator_example/$', 'lazy_decorator_example',
-        name='facebook_lazy_decorator_example'),
-    url(r'^decorator_example/$', 'decorator_example',
-        name='facebook_decorator_example'),
-    url(
-        r'^decorator_example_scope/$', 'decorator_example_scope',
-        name='facebook_decorator_example_scope'),
-    url(r'^wall_post/$',
-        'wall_post', name='facebook_wall_post'),
-    url(r'^checkins/$',
-        'checkins', name='facebook_checkins'),
-    url(r'^image_upload/$',
-        'image_upload', name='facebook_image_upload'),
-    url(r'^canvas/$', 'canvas', name='facebook_canvas'),
-    url(r'^page_tab/$',
-        'page_tab', name='facebook_page_tab'),
-    url(r'^open_graph_beta/$', 'open_graph_beta',
-        name='facebook_open_graph_beta'),
-    url(r'^remove_og_share/$', 'remove_og_share',
-        name='facebook_remove_og_share'),
-)
 
+dev_patterns = [
+url(r'^lazy_decorator_example/$', lazy_decorator_example,name='facebook_lazy_decorator_example'),
+url(r'^decorator_example/$', decorator_example,name='facebook_decorator_example'),
+url(r'^decorator_example_scope/$', decorator_example_scope,name='facebook_decorator_example_scope'),
+url(r'^wall_post/$',wall_post, name='facebook_wall_post'),
+url(r'^checkins/$',checkins, name='facebook_checkins'),
+url(r'^image_upload/$',image_upload, name='facebook_image_upload'),
+url(r'^canvas/$', canvas, name='facebook_canvas'),
+url(r'^page_tab/$',page_tab, name='facebook_page_tab'),
+url(r'^open_graph_beta/$', open_graph_beta,name='facebook_open_graph_beta'),
+url(r'^remove_og_share/$',remove_og_share,name='facebook_remove_og_share')
+]
 # when developing enable the example views
 if settings.DEBUG or getattr(settings, 'TESTING', False):
     # only enable example views while developing
